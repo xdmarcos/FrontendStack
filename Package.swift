d517a5555c51e9eb2a516111f4a266828f4f7ffa
xdmarcos <xdmgzdev@gmail.com>
2021-03-20 14:33:57 +0000

    Remove wrong scheme targets

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ import PackageDescription
 
 let package = Package(
   name: "FrontendStackStyle",
+  platforms: [
+    .iOS(.v11), .tvOS(.v11),
+  ],
   products: [
     .library(name: "FrontendStackStyle", targets: ["FrontendStackStyle"]),
   ],
