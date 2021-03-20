607e0ba026cc337d184b3617f52e38d828e417a4
xdmarcos <xdmgzdev@gmail.com>
2021-03-20 19:48:29 +0000

    Update FSFont.swift

--- Sources/FrontendStackStyle/FSFont.swift
+++ Sources/FrontendStackStyle/FSFont.swift
@@ -9,51 +9,51 @@ import UIKit
 
 public enum FSFont {  
   static func black(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-Black", size: ofSize)
+    loadFont(name: "Roboto-Black", size: ofSize)
   }
 
   static func blackItalic(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-BlackItalic", size: ofSize)
+    loadFont(name: "Roboto-BlackItalic", size: ofSize)
   }
 
   static func bold(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-Bold", size: ofSize)
+    loadFont(name: "Roboto-Bold", size: ofSize)
   }
 
   static func boldItalic(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-BoldItalic", size: ofSize)
+    loadFont(name: "Roboto-BoldItalic", size: ofSize)
   }
 
   static func italic(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-Italic", size: ofSize)
+    loadFont(name: "Roboto-Italic", size: ofSize)
   }
 
   static func light(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-Light", size: ofSize)
+    loadFont(name: "Roboto-Light", size: ofSize)
   }
 
   static func lightItalic(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-LightItalic", size: ofSize)
+    loadFont(name: "Roboto-LightItalic", size: ofSize)
   }
 
   static func medium(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-Medium", size: ofSize)
+    loadFont(name: "Roboto-Medium", size: ofSize)
   }
 
   static func mediumItalic(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-MediumItalic", size: ofSize)
+    loadFont(name: "Roboto-MediumItalic", size: ofSize)
   }
 
   static func regular(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-Regular", size: ofSize)
+    loadFont(name: "Roboto-Regular", size: ofSize)
   }
 
   static func thin(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-Thin", size: ofSize)
+    loadFont(name: "Roboto-Thin", size: ofSize)
   }
 
   static func thinItalic(_ ofSize: CGFloat) -> UIFont {
-    return Self.loadFont(name: "Roboto-ThinItalic", size: ofSize)
+    loadFont(name: "Roboto-ThinItalic", size: ofSize)
   }
 }
 
